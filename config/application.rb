2f93b579839cc7217a590d4d9a97e1583f44f9a1
zhuang-ced <sho.sc@icloud.com>
2020-12-05 18:38:41 +0900

    アプリ名誤字の修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Frima29643
+module Furima29643
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "frima_29643_production"
+  # config.active_job.queue_name_prefix = "furima_29643_production"
 
   config.action_mailer.perform_caching = false
 
