e1b594b320b6d2508998cd04a938a1945104e124
zhuang-ced <sho.sc@icloud.com>
2020-12-19 14:39:13 +0900

    ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-def create
-  User.create(user_params)
-end
-
-
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
