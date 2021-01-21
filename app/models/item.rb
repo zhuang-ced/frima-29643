035fd9780fc9dfa4dff3e673851a7c5728857d88
zhuang-ced <sho.sc@icloud.com>
2021-01-21 16:36:42 +0900

    出品した商品の情報をテーブルに保存できる記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,9 @@ class Item < ApplicationRecord
   validates :image, :item_name, :explanation, :category, :status_id, :delivery_fee_id, :shipping_area_id, :days_to_ship_id, :price, presence: true
   validates :item_name, length: { maximum: 40}
   validates :explanation, length: { maximum: 1000}
+  validates :price, numericality: { greater_than: 300}
+  validates :price, numericality: { less_than: 9999999}
+  validates :price, numericality: true
 
   with_options numericality: {other_than: 1 } do
     validates :category_id, :status_id, :category_id, :delivery_fee_id, :shipping_area_id, :days_to_ship_id
